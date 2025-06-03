b8cbd8c861cdbe5673841d5bed31a9c1890951a7
Gachiza <gachiza17@gmail.com>
2025-06-03 17:04:07 +0200

    adding the routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,20 @@ const port = 3000;
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send('Hello World!')
+  res.send('Home')
+});
+
+app.get('/products', (req,res) => {
+  res.send('List of products');
+});
+
+app.get('/products/:id', (req,res) => {
+  console.log(req);
+  res.send(' the product');
+});
+
+app.post('/products', (req,res) => {
+  res.send('New product created');
 });
 
 app.listen(port, () => {
