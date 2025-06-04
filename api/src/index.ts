2c0e3ec094eb6c2280d78df09a94fe97032266d8
Gachiza <gachiza17@gmail.com>
2025-06-04 10:38:15 +0200

    middlware setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,15 @@
-import express, {json} from 'express';
+import express, {json, urlencoded} from 'express';
 import productsRoutes from './routes/products/index'
 
 const port = 3000;
 const app = express();
+
 app.use(json());
+app.use(urlencoded({extended: false}));
 
 app.get('/', (req,res) => {
   res.send('Home')
 });
-
 
 app.use('/products', productsRoutes);
 
