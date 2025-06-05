637e338475b2762b363634df9d474e9c67c5d43a
Gachiza <gachiza17@gmail.com>
2025-06-05 15:33:03 +0200

    updated index.ts

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,20 +1,6 @@
 import { Router } from 'express';
 import { listProducts, getProductById, createProduct, updateProduct, deleteProduct } from './productsController';
-import { productsTable } from '../../db/productSchema';
 import {validateData} from '../../middlewares/validationMiddlewares';
-
-
-import { createInsertSchema, createSelectSchema} from 'drizzle-zod';
-
-import {z} from 'zod';
-
-// const createProductSchema = z.object({
-//   name: z.string(),
-//   price: z.number({message: 'price should be a number'}),
-// });
-
-export const createProductSchema = createInsertSchema(productsTable);
-
 
 //Products endpoints
 const router = Router();
